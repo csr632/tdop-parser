2c6c99ccc4af441f616c453691d992bb3bb2c4c5
csr632 <632882184@qq.com>
2022-02-19 10:43:38 +0800

    improve tests

--- src/tests/parser.test.ts
+++ src/tests/parser.test.ts
@@ -1,98 +1,106 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { createParser } from "../parser";
 import { createScanner } from "../scanner";
 
-test("parse -1+2*6/3+5", () => {
-  const parser = createParser(createScanner("-1+2*6/3+5"));
-  const res = parser.parseProgram();
-  expect(res).toMatchSnapshot();
+describe("basic", () => {
+  test("-1+2*6/3+5", () => {
+    const parser = createParser(createScanner("-1+2*6/3+5"));
+    const res = parser.parseProgram();
+    expect(res).toMatchSnapshot();
+  });
+
+  test("1 + 2^3^4*5-6", () => {
+    const parser = createParser(createScanner("1 + 2^3^4*5-6"));
+    const res = parser.parseProgram();
+    expect(res).toMatchSnapshot();
+    // console.log(JSON.stringify(res, null, 2))
+  });
 });
 
-test("parse 1 + 2^3^4*5-6", () => {
-  const parser = createParser(createScanner("1 + 2^3^4*5-6"));
-  const res = parser.parseProgram();
-  expect(res).toMatchSnapshot();
-  // console.log(JSON.stringify(res, null, 2))
-});
+describe("conditional operation", () => {
+  test("true ? 1 : 2", () => {
+    const parser = createParser(createScanner("true ? 1 : 2"));
+    const res = parser.parseProgram();
+    expect(res).toMatchSnapshot();
+  });
 
-test("parse true ? 1 : 2", () => {
-  const parser = createParser(createScanner("true ? 1 : 2"));
-  const res = parser.parseProgram();
-  expect(res).toMatchSnapshot();
-});
+  test("1+ 2  *3 ? 1+1 : 2  -1", () => {
+    const parser = createParser(createScanner("1+ 2  *3 ? 1+1 : 2  -1"));
+    const res = parser.parseProgram();
+    expect(res).toMatchSnapshot();
+  });
 
-test("parse 1+ 2  *3 ? 1+1 : 2  -1", () => {
-  const parser = createParser(createScanner("1+ 2  *3 ? 1+1 : 2  -1"));
-  const res = parser.parseProgram();
-  expect(res).toMatchSnapshot();
-});
-
-test("parse conditional chains", () => {
-  const parser = createParser(
-    createScanner(`
+  test("conditional chains", () => {
+    const parser = createParser(
+      createScanner(`
   condition1 ? value1
   : condition2 ? value2
   : condition3 ? value3
   : value4`)
-  );
-  const res = parser.parseProgram();
-  expect(res).toMatchSnapshot();
-});
-test("parse conditional chains", () => {
-  const parser = createParser(
-    createScanner(`
+    );
+    const res = parser.parseProgram();
+    expect(res).toMatchSnapshot();
+  });
+  test("conditional chains", () => {
+    const parser = createParser(
+      createScanner(`
   condition1
     ? condition2 ? true2 : false2
     : condition3
       ? condition4 ? true4 : false4
       : condition5 ? true5 : false5
   `)
-  );
-  const res = parser.parseProgram();
-  expect(res).toMatchSnapshot();
+    );
+    const res = parser.parseProgram();
+    expect(res).toMatchSnapshot();
+  });
+
+  test("true ? 1 ; 2", () => {
+    expect(() => {
+      const parser = createParser(createScanner("true ? 1 ; 2"));
+      parser.parseProgram();
+    }).toThrowError("expect token : but got ;");
+  });
 });
 
-test("parse true ? 1 ; 2", () => {
-  expect(() => {
-    const parser = createParser(createScanner("true ? 1 ; 2"));
-    parser.parseProgram();
-  }).toThrowError("expect token : but got ;");
+describe("error handling", () => {
+  test("1 + ", () => {
+    expect(() => {
+      const parser = createParser(createScanner("1 + "));
+      parser.parseProgram();
+    }).toThrowError("expect token but found none");
+  });
+
+  test("1 2", () => {
+    const parser = createParser(createScanner("1 2"));
+    const res = parser.parseProgram();
+    expect(res).toMatchSnapshot();
+  });
 });
 
-test("parse 1 + ", () => {
-  expect(() => {
-    const parser = createParser(createScanner("1 + "));
-    parser.parseProgram();
-  }).toThrowError("expect token but found none");
-});
+describe("parenthesis", () => {
+  test("2*(3-1)", () => {
+    const parser = createParser(createScanner("2*(3-1)"));
+    const res = parser.parseProgram();
+    expect(res).toMatchSnapshot();
+  });
 
-test("parse 1 2", () => {
-  const parser = createParser(createScanner("1 2"));
-  const res = parser.parseProgram();
-  expect(res).toMatchSnapshot();
-});
+  test("((1))+1", () => {
+    const parser = createParser(createScanner("((1))+1"));
+    const res = parser.parseProgram();
+    expect(res).toMatchSnapshot();
+  });
 
-test("parse 2*(3-1)", () => {
-  const parser = createParser(createScanner("2*(3-1)"));
-  const res = parser.parseProgram();
-  expect(res).toMatchSnapshot();
-});
+  test("2*(9*(((2))+1))-1", () => {
+    const parser = createParser(createScanner("2*(9*(((2))+1))-1"));
+    const res = parser.parseProgram();
+    expect(res).toMatchSnapshot();
+  });
 
-test("parse ((1))+1", () => {
-  const parser = createParser(createScanner("((1))+1"));
-  const res = parser.parseProgram();
-  expect(res).toMatchSnapshot();
-});
-
-test("parse 2*(9*(((2))+1))-1", () => {
-  const parser = createParser(createScanner("2*(9*(((2))+1))-1"));
-  const res = parser.parseProgram();
-  expect(res).toMatchSnapshot();
-});
-
-test("parse 2*(3 1)", () => {
-  expect(() => {
-    const parser = createParser(createScanner("2*(3 1)"));
-    parser.parseProgram();
-  }).toThrowError("expect token ) but got 1");
+  test("2*(3 1)", () => {
+    expect(() => {
+      const parser = createParser(createScanner("2*(3 1)"));
+      parser.parseProgram();
+    }).toThrowError("expect token ) but got 1");
+  });
 });
